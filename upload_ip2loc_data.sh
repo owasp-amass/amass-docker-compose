c487627fa33aab2406256705b042c9e710baf6bf
caffix <caffix@users.noreply.github.com>
2024-03-06 21:41:51 -0500

    working on the routines supporting the IP to geo lookups

--- upload_ip2loc_data.sh
+++ upload_ip2loc_data.sh
@@ -7,21 +7,26 @@ PGPASSWORD="grafana" psql -v ON_ERROR_STOP=1 -h localhost -U "grafana" --dbname 
     DECLARE
         _ip TEXT;
         _ips TEXT[];
-        _ipbig BIGINT;
+        _ipdec DECIMAL(39,0);
         var_r RECORD;
     BEGIN
         _ips = string_to_array(\$1,',');
         
         FOREACH _ip IN ARRAY _ips
         LOOP 
-            _ipbig = inet_to_bigint(_ip::inet);
+            IF family(_ip::inet) = 4 THEN
+                _ipdec = ipv4_to_decimal(_ip::inet);
+            ELSIF family(_ip::inet) = 6 THEN
+                _ipdec = ipv6_to_decimal(_ip::inet);
+            END;
 
             FOR var_r IN (
                 WITH addr(ip_addr) AS (VALUES (_ip))
-                SELECT addr.ip_addr AS addr, geo.latitude AS lat, geo.longitude AS long 
-                FROM (SELECT * FROM ip_geo WHERE _ipbig <= ip_to) AS geo 
-                JOIN addr ON 1=1 
-                WHERE _ipbig >= ip_from AND country_code != '-' LIMIT 1
+                SELECT addr.ip_addr AS addr, geo.latitude AS "lat", geo.longitude AS "long" 
+                FROM (SELECT ip_geo.latitude, ip_geo.longitude FROM ip_geo 
+                WHERE _ipbig >= ip_from AND country_code != '-' 
+                ORDER BY ip_from DESC LIMIT 1) AS geo 
+                JOIN addr ON 1=1
             ) 
             LOOP ip_addr := var_r.addr;
                 latitude := var_r.lat;
@@ -30,13 +35,59 @@ PGPASSWORD="grafana" psql -v ON_ERROR_STOP=1 -h localhost -U "grafana" --dbname 
             END LOOP;
         END LOOP;
     END
+    \$BODY\$
+    LANGUAGE plpgsql;
+
+    CREATE OR REPLACE FUNCTION ipv4_to_decimal(inet) RETURNS DECIMAL(39,0) AS 
+    \$BODY\$ 
+        SELECT \$1 - '0.0.0.0'::inet
+    \$BODY\$ 
+    LANGUAGE sql strict immutable;
+
+    CREATE OR REPLACE FUNCTION ipv6_to_decimal(inet) RETURNS DECIMAL(39,0) AS
+    \$BODY\$ 
+    DECLARE
+        _group TEXT;
+        _groups TEXT[];
+        _ipbig BIGINT;
+        var_r RECORD;
+    BEGIN
+        _groups = string_to_array(host(\$1),':');
+        
+        FOREACH _group IN ARRAY _groups
+        LOOP 
+            _ipbig = hex_to_bigint(_group);
+        END LOOP;
+    END
     \$BODY\$ LANGUAGE plpgsql;
 
-    CREATE OR REPLACE FUNCTION inet_to_bigint(inet) RETURNS bigint AS \$BODY\$ 
-        SELECT \$1 - '0.0.0.0'::inet WHERE family(\$1) = 4
-        UNION ALL
-        SELECT '0'::bigint WHERE family(\$1) = 6
-    \$BODY\$ LANGUAGE sql strict immutable;
+    CREATE OR REPLACE FUNCTION ipv6_to_decimal(inet) RETURNS DECIMAL(39,0) AS
+    \$BODY\$ 
+    DECLARE
+        _group TEXT;
+        _groups TEXT[];
+        _ipbig BIGINT;
+        var_r RECORD;
+    BEGIN
+        _groups = string_to_array(host(\$1),':');
+        
+        FOREACH _group IN ARRAY _groups
+        LOOP 
+            _ipbig = hex_to_bigint(_group);
+        END LOOP;
+    END
+    \$BODY\$ LANGUAGE plpgsql;
+
+    CREATE OR REPLACE FUNCTION hex_to_bigint(hexval varchar) RETURNS bigint AS 
+    \$BODY\$
+    DECLARE
+        result bigint;
+    BEGIN
+        EXECUTE 'SELECT x' || quote_literal(hexval) || '::bigint' INTO result;
+        RETURN result;
+    END;
+    \$BODY\$ 
+    LANGUAGE plpgsql IMMUTABLE STRICT;
 
     GRANT execute ON FUNCTION inet_to_bigint(inet) TO public;
 
